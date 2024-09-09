106c088c82305b439647b1a3e779808ac8a8ab8c
prasanna.natarajan <prasanna@npras.in>
2024-09-09 09:14:45 +0530

    3.2 done

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
+var cfg config
+
 func main() {
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
+
+	logOpts := &slog.HandlerOptions{
+		Level:     slog.LevelDebug,
+		AddSource: true,
+	}
+	logger := slog.New(slog.NewTextHandler(os.Stdout, logOpts))
+
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /{$}", home)
@@ -16,8 +35,9 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting server on :4000")
+	logger.Info("starting server on", slog.String("port", cfg.addr))
 
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	err := http.ListenAndServe(cfg.addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
