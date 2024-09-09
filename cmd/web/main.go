a8034087195ecf3bf7e061dc615c714dcb327f5d
prasanna.natarajan <prasanna@npras.in>
2024-09-09 09:58:09 +0530

    3.3 done (with error)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,32 +12,40 @@ type config struct {
 	staticDir string
 }
 
-var cfg config
+type application struct {
+	logger *slog.Logger
+}
 
-func main() {
-	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
-	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
-	flag.Parse()
-
+func NewLogger() *slog.Logger {
 	logOpts := &slog.HandlerOptions{
 		Level:     slog.LevelDebug,
 		AddSource: true,
 	}
-	logger := slog.New(slog.NewTextHandler(os.Stdout, logOpts))
+	return slog.New(slog.NewTextHandler(os.Stdout, logOpts))
+}
+
+func main() {
+	cfg := config{}
+
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
+
+	app := &application{logger: NewLogger()}
 
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	logger.Info("starting server on", slog.String("port", cfg.addr))
+	app.logger.Info("starting server on", slog.String("port", cfg.addr))
 
 	err := http.ListenAndServe(cfg.addr, mux)
-	logger.Error(err.Error())
+	app.logger.Error(err.Error())
 	os.Exit(1)
 }
