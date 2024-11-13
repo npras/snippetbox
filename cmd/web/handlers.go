d184b324567621f2080bdd0606541f2a82a7a2b5
prasanna.natarajan <prasanna@npras.in>
2024-11-13 07:12:53 +0530

    7.4 done

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -51,11 +51,22 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 //
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	app.logger.Info("RENDERING CREATE SNIPPET")
 	data := app.newTemplateData(r)
+	data.Form = snippetCreateForm{
+		ExpiresAt: 365,
+	}
 	app.render(w, r, http.StatusOK, "create.tmpl.html", data)
 }
 
 //
+
+type snippetCreateForm struct {
+	Title       string
+	Content     string
+	ExpiresAt   int
+	FieldErrors map[string]string
+}
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	app.logger.Info("CREATING SNIPPET")
@@ -65,41 +76,49 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
 	expiresAt, err := strconv.Atoi(r.PostForm.Get("expires"))
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
-	fieldErrors := make(map[string]string)
-
-	if strings.TrimSpace(title) == "" {
-		fieldErrors["title"] = "title can't be blank"
-	} else if utf8.RuneCountInString(title) > 100 {
-		fieldErrors["title"] = "title should be less than 100 chars"
+	form := snippetCreateForm{
+		Title:       r.PostForm.Get("title"),
+		Content:     r.PostForm.Get("content"),
+		ExpiresAt:   expiresAt,
+		FieldErrors: map[string]string{},
 	}
 
-	if strings.TrimSpace(content) == "" {
-		fieldErrors["content"] = "content can't be blank"
-	}
-
-	validExpiresAt := []int{1, 7, 365}
-	if !slices.Contains(validExpiresAt, expiresAt) {
-		fieldErrors["expiresAt"] = "expiresAt should be one of 1, 7, 365"
-	}
-
-	if len(fieldErrors) > 0 {
-		fmt.Fprint(w, fieldErrors)
+	validateSnippetCreateFields(form.FieldErrors, form.Title, form.Content, form.ExpiresAt)
+	if len(form.FieldErrors) > 0 {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusOK, "create.tmpl.html", data)
 		return
 	}
 
-	id, err := app.snippet.Insert(title, content, expiresAt)
+	id, err := app.snippet.Insert(form.Title, form.Content, form.ExpiresAt)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
 	redirectToURL := fmt.Sprintf("/snippet/view/%d", id)
 	http.Redirect(w, r, redirectToURL, http.StatusSeeOther)
+}
+
+func validateSnippetCreateFields(m map[string]string, title, content string, expiresAt int) {
+	if strings.TrimSpace(title) == "" {
+		m["title"] = "title can't be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		m["title"] = "title should be less than 100 chars"
+	}
+
+	if strings.TrimSpace(content) == "" {
+		m["content"] = "content can't be blank"
+	}
+
+	validExpiresAt := []int{1, 7, 365}
+	if !slices.Contains(validExpiresAt, expiresAt) {
+		m["expiresAt"] = "expiresAt should be one of 1, 7, 365"
+	}
 }
